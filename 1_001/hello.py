053a481e1f4065b862fc8c21b52f7f2eda647a6b
SAM\fins6 <fins6789@gmail.com>
2023-06-04 20:59:36 +0900

    0604 python basic

---
+++ 1_001/hello.py
@@ -0,0 +1,45 @@
+str = 'Hello python'
+print(str)
+print('Hello')
+print(1+1)
+
+num = 10*10
+print(num)
+
+name = 'jake'
+age = 30
+
+print(f'My name is {name}, age is {age}')
+print('My name is {}, age is {}'.format(name, age))
+print('My name is %s, age is %d' %(name, age))
+
+def add(a,b):
+    return print(a+b)
+
+add(10,12)
+
+'''
+함수정의
+프린트 방법
+'''
+def greet(name):
+    message = 'Hi~ ' + name + ' sir'
+    return message
+
+#1
+print(greet('jake'))
+#2
+result = greet('jake')
+print(result)
+
+def greet2(name):
+    return 'Hi~ ' + name + ' sir'
+
+#1
+print(greet2('jake'))
+#2
+result2 = greet2('jake')
+print(result2)
+
+print(f'name is {name}, age is {age}')
+
